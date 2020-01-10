af4864fabfe25b6670cb12c38a8780bf1f681ba7
Harold Portocarrero <harold.port@gmail.com>
2020-01-09 20:44:25 -0500

    Schema and documentation with CoreAPI

--- app/blog_project/urls.py
+++ app/blog_project/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 from rest_auth import views
+
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +17,7 @@ urlpatterns = [
     path('api/v1/rest-auth/password/reset/confirm/<uidb64>/<token>/',
          views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title=API_TITLE,
+                                    description=API_DESCRIPTION)),
+    path('schema/', schema_view),
 ]
